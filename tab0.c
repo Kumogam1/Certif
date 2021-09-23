2610b35afc6bb47b608dca7ccb9f668163257f66
GU Edouard <e.gu@pcj214.psi.ad.unistra.fr>
2021-09-23 12:24:09 +0200

    tableau

--- tab0.c
+++ tab0.c
@@ -1,8 +1,9 @@
 /*@ requires 0 < n && \valid(t+(0..n-1));
-@ ensures \result == 0 <==> (\forall integer i; 0 <= i < n ==> t[i] == 0);
+@ ensures \result == 0 ==> (\forall integer i; 0 <= i < n ==> t[i] == 0);
 @*/
 int all_zeros(int t[], int n) {
     /*@ loop invariant 0 <= i < n;
+      @ loop invariant \forall int j; 0<= j < i ==> t[j] == 0;
       @ loop variant n - i;
       @*/
     for(int i = 0; i< n; i++){
