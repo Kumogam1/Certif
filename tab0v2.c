f75ed416842222a9e9061d8f481905a4ebb93cda
GU Edouard <e.gu@pcj214.psi.ad.unistra.fr>
2021-09-23 12:27:09 +0200

    tableau

---
+++ tab0v2.c
@@ -0,0 +1,21 @@
+/*@
+requires \valid(t+(0..n-1));
+requires n >= 0;
+assigns t[0..n-1];
+ensures \forall int i; 0 <= i < n ==> t[i] == 0;
+*/
+int all_zeros(int* t, int n)
+{
+    /*@
+    loop invariant 0 <= i <= n;
+    loop invariant \forall int j; 0 <= j < i ==> t[j] == 0;
+    loop assigns i, t[0..n-1];
+    loop variant n - i;
+    */
+    for(int i = 0; i < n; ++i)
+    {
+        t[i] = 0;
+    }
+
+    return 0;
+}
